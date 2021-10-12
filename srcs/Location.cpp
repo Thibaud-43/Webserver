e7c0a4ba88a0352604cd89181a22d59a0aff7e2d
Tom Vacherat <tvacherat@gmail.com>
2021-10-12 10:01:17 +0200

    main branch updated

---
+++ srcs/Location.cpp
@@ -0,0 +1,55 @@
+#include "Location.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Location::Location()
+{
+}
+
+Location::Location( const Location & src )
+{
+}
+
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Location::~Location()
+{
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Location &				Location::operator=( Location const & rhs )
+{
+	//if ( this != &rhs )
+	//{
+		//this->_value = rhs.getValue();
+	//}
+	return *this;
+}
+
+std::ostream &			operator<<( std::ostream & o, Location const & i )
+{
+	//o << "Value = " << i.getValue();
+	return o;
+}
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+
+/* ************************************************************************** */
