b60e2a191cad772331c2429715b20d84ad60158b
UnDesSix <mlarboul@student.42.fr>
2021-10-18 22:37:08 +0200

    [TEST] commenting my tests and cleaning main file

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,11 +1,6 @@
 #include "Cluster.hpp"
 #include "Tree.hpp"
 
-void    functionIte(std::vector<std::string>::iterator &it)
-{
-    for (unsigned int i = 0; i < 2; i++)
-        it++;
-}
 int main(int argc, char const *argv[])
 {
     Cluster test;
@@ -15,8 +10,9 @@ int main(int argc, char const *argv[])
     // std::cout << "--------PARSING TESTS--------" << std::endl << std::endl;
     // Tree   tree("test.conf");
 
-    // std::cout << tree;
-    // tree.parseCluster();
 
-    // return 0;
+    // tree.parseCluster();
+    // std::cout << tree;
+
+    return 0;
 }
