ee03b1b5ad26dc0f71ff7f9f6fe9fef986b1c02a
mlarboul <mlarboul@student.42.fr>
2021-10-21 14:10:10 +0200

    [TEST] print BST

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,28 @@
 #include "Cluster.hpp"
 #include "Tree.hpp"
 
+void printBT(const std::string& prefix, const Node* node, bool isLeft)
+{
+    if( node != nullptr )
+    {
+        std::cout << prefix;
+
+        std::cout << (isLeft ? "├──" : "└──" );
+
+        // print the value of the node
+        std::cout << node->getType() << std::endl;
+
+        // enter the next tree level - left and right branch
+        printBT( prefix + (isLeft ? "│   " : "    "), node->getRight(), true);
+        printBT( prefix + (isLeft ? "│   " : "    "), node->getLeft(), false);
+    }
+}
+
+void printBT(const Node* node)
+{
+    printBT("", node, false);    
+}
+
 int main(int argc, char const *argv[])
 {
     // Cluster test;
@@ -13,6 +35,9 @@ int main(int argc, char const *argv[])
 
     tree.parseCluster();
     std::cout << tree;
+    std::cout << std::endl << std::endl;
+    printBT(tree.getRoot());
+
 
     return 0;
 }
