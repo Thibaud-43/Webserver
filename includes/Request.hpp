53cccee446f3b465310a207200f93bd304f80dea
Thibaud <thibaud.rouchon@yahoo.fr>
2021-11-04 12:23:26 +0100

    push for response rework

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -5,6 +5,7 @@
 # include "Client.hpp"
 # include "Server.hpp"
 # include "Location.hpp"
+# include "Response.hpp"
 
 class Request: public Client
 {
@@ -24,6 +25,15 @@ class Request: public Client
 		Location	*m_location;
 		std::string	m_path;
 		header_type	m_header;
+	private:
+		void		_bufferToRequestLine(void);
+		void		_bufferToHeader(void);
+		void		_bufferToHeaderLine(std::string & token);
+		void		_printHeader(void);
+		bool		_checkHeader(void);
+
+
+
 
 };
 
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -35,9 +35,136 @@ Request::~Request()
 ** --------------------------------- METHODS ----------------------------------
 */
 
+void			Request::_bufferToRequestLine(void)
+{
+	std::string delimiter1 = "\r\n";
+	std::string delimiter2 = " ";
+	std::string s = m_buff.substr(0, m_buff.find(delimiter1));
+	size_t	count = 0;
+	size_t pos = 0;
+	std::string	token;
+	while ((pos = s.find(delimiter2)) != std::string::npos) 
+	{
+
+		token = s.substr(0, pos);
+		if (count == 0)
+		{
+			m_header.insert(std::pair<std::string, std::string>("method", token));
+		}
+		else if(count == 1)
+		{
+			m_header.insert(std::pair<std::string, std::string>("uri", token));
+		}
+		count++;
+		token.clear();
+		s.erase(0, pos + delimiter2.length());
+	}
+	m_header.insert(std::pair<std::string, std::string>("protocol", s));
+	m_buff.erase(0, m_buff.find(delimiter1) + delimiter1.length());
+}	
+
+void			Request::_bufferToHeaderLine(std::string & token)
+{
+	std::string delimiter = ": ";
+	size_t		pos = token.find(delimiter);
+	m_header.insert(std::pair<std::string, std::string>(token.substr(0, pos), token.substr(pos + delimiter.length(), token.length() - 1)));
+}
+
+void			Request::_bufferToHeader(void)
+{
+	std::string delimiter = "\r\n";
+	std::string delimiter2 = "\r\n\r\n";
+	size_t pos = 0;
+	std::string	token;
+
+	if (m_buff == delimiter)
+	{
+		m_buff = "";
+		return ;
+	}
+	while ((pos = m_buff.find(delimiter)) != std::string::npos && pos != m_buff.find(delimiter2)) 
+	{
+		token = m_buff.substr(0, pos);
+		_bufferToHeaderLine(token);
+		m_buff.erase(0, pos + delimiter.length());
+	}
+	if (m_buff.find(delimiter2) != std::string::npos)
+	{
+		token = m_buff.substr(0, pos);
+		_bufferToHeaderLine(token);
+		m_buff.erase(0, pos + delimiter2.length());
+		return ;
+	}
+	else
+	{
+		return ;
+	}
+}
+
+void			Request::_printHeader(void)
+{
+	std::cout << std::endl << std::endl << "MAP REQUEST HEADER" << std::endl;
+	for (std::map<std::string, std::string>::iterator it = m_header.begin(); it != m_header.end(); it++)
+	{
+		std::cout << "[" << it->first << "]='" << it->second << "'" << std::endl << std::endl; 
+	}
+}
+
+bool	Request::_checkHeader(void)
+{
+	if (_checkRequestLine() == false || _checkHost() == false)
+		return (false);
+	_linkServer(servers);
+	_linkLocation();
+	_linkPath();
+	if (!m_location->isAllowed(m_header["method"]))
+	{
+		Response::send_error("400", m_client, &m_server->getParams());
+		return (false);
+	}
+	return (true);
+}
+
+bool	Request::_checkHost(void)
+{
+	if (m_header["Host"].empty())
+	{
+		Response::send_error("400", m_client);
+		return (false);
+	}
+	return (true);
+}
+
+bool	Request::_checkRequestLine(void)
+{
+	if (m_header.empty() || m_header["protocol"].empty() || m_header["method"].empty() || m_header["uri"].empty())
+	{
+		Response::send_error("400", m_client);
+		return (false);
+	}
+	if (m_header["protocol"] != PROTOCOL)
+	{
+		Response::send_error("505", m_client);
+		return (false);
+	}
+	if (m_header["method"] != "GET" && m_header["method"] != "POST" && m_header["method"] != "DELETE")
+	{
+		Response::send_error("400", m_client);
+		return (false);
+	}
+	return (true);
+}
+
 bool	Request::execute(ASocket *ptr)
 {
-	// FILL buffer
+	if (!_fillBuffer())
+		return false;
+	if (m_buff.find("\r\n\r\n") == std::string::npos)
+		return true;
+	_bufferToRequestLine();
+	_bufferToHeader();
+	_printHeader();
+	
 	// IF /r/n/r/n in m_buff -> PARSING HEADER
 }
 
